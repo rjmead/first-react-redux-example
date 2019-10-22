f6bb8d4628d6e4c036e7a0abe937b317aab19948
bmead92 <bmead92@gmail.com>
2019-10-21 22:13:19 -0400

    update readme

--- src/components/SongDetail.js
+++ src/components/SongDetail.js
@@ -1,8 +1,25 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const SongDetail = () => {
-    return <div>SongDetail</div>
+const SongDetail = ({ song }) => {
+    if (!song) {
+        return <div>Select a song</div>
+    }
+
+    return (
+        <div>
+            <h3>Details for:</h3>
+            <p>
+                Title: {song.title}
+                <br/>
+                Duration: {song.duration}
+            </p>
+        </div>
+    )
 }
 
-export default SongDetail
+const mapStateToProps = (state) => {
+    return { song: state.selectedSong }
+}
+
+export default connect(mapStateToProps)(SongDetail)
